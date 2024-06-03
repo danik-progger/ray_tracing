a04e3a3aef5f161b912fd5a512b088c14a410f61
danik-progger <lyatorovskiy@gmail.com>
2024-06-03 18:18:13 +0300

    chapter 10

--- src/classes/material.h
+++ src/classes/material.h
@@ -34,18 +34,21 @@ class LambertianMaterial : public Material {
 
 class Metal : public Material {
  public:
-  Metal(const Color& albedo) : albedo(albedo) {}
+  Metal(const Color& albedo, double fuzz)
+      : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
 
   bool scatter(const Ray& r_in, const HitRecord& rec, Color& attenuation,
                Ray& scattered) const override {
     Vec3 reflected = reflect(r_in.direction(), rec.normal);
+    reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
     scattered = Ray(rec.p, reflected);
     attenuation = albedo;
-    return true;
+    return (dot(scattered.direction(), rec.normal) > 0);
   }
 
  private:
   Color albedo;
+  double fuzz;
 };
 
 #endif  // MATERIAL_H
